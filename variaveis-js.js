5707b8357c3aad0e4e094040960f629a95e3de10
Bruno Pina <112709471+Pina98@users.noreply.github.com>
2022-09-09 12:11:12 -0300

    Add files via upload

---
+++ variaveis-js.js
@@ -0,0 +1,7 @@
+const prompt = require('prompt-sync')();
+
+const firstName = prompt('Type your first name: ');
+const weight = prompt('Type your weight: ');
+const height = prompt('Type your height: ');
+const date = prompt('Type hospitalization date: ');
+const option = prompt('Are you diabetic? ');
